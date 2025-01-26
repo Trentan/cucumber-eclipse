1f0d1d8589036899583240cf92bd04faa060430e
Trentan Healey <trentanh@gmail.com>
2025-01-26 11:46:40 +1000

    examples

---
+++ release/example/src/main/java/cucumber/examples/calculator/DateCalculator.java
@@ -0,0 +1,17 @@
+package cucumber.examples.calculator;
+
+import java.time.LocalDate;
+
+public class DateCalculator {
+
+    private final LocalDate now;
+
+    public DateCalculator(LocalDate now) {
+        this.now = now;
+    }
+
+    public String isDateInThePast(LocalDate date) {
+        return (date.isBefore(now)) ? "yes" : "no";
+    }
+
+}
